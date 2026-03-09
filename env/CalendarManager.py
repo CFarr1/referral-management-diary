76de0359dfb34201e0cb57dbc3a9bf9f319ac01d
Calstar5 <callumfarr96@gmail.com>
2026-03-09 16:54:40 +0000

    final changes for the day

--- env/CalendarManager.py
+++ env/CalendarManager.py
@@ -5,5 +5,5 @@ events = []
 
 # Example events – adjust dates to be near your current date if you want to see them
 events.append(Event("Steve", datetime(2026, 3, 4, 10, 30), timedelta(minutes=120)))
-events.append(Event("Barry", datetime(2026, 3, 5, 14, 0), timedelta(minutes=60)))
+events.append(Event("Barry", datetime(2026, 3, 5, 14, 21), timedelta(minutes=60)))
 events.append(Event("bob", datetime(2026, 3, 6, 14, 0), timedelta(minutes=60)))
